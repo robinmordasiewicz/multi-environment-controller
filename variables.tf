3d9efe8ed9a76f57b81f071bc72360c831b0785b
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-09-16 12:58:59 -0400

    updating fortigate config

--- variables.tf
+++ variables.tf
@@ -3,30 +3,31 @@ variable "environments" {
     name                = string
     ARM_SUBSCRIPTION_ID = string
     AZURE_DEPLOYED      = string
+    OWNER_EMAIL         = string
   }))
-  default = []
+  default     = []
+  sensitive   = true
+  description = "List of applications that will be used to create github branches and environments"
 }
 
 variable "AZURE_REGION" {
   type        = string
-  description = "(Optional) Azure region to use for storage account. Defaults to Canada Central."
-  default     = "CanadaCentral"
-}
-
-variable "AZURE_USERNAME" {
-  type = string
+  description = "Azure region to use for storage account."
+  sensitive   = true
 }
 
 variable "ARM_TENANT_ID" {
-  type = string
+  type        = string
+  sensitive   = true
+  description = "Used for OIDC authentication"
 }
 
 variable "REPOSITORY_TOKEN" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "(Required) The github PAT for the repository."
 }
 
-# The GitHub repo where we'll be creating secrets and environments
 variable "REPOSITORY_NAME" {
   type        = string
   description = "(Required) The name of the repository we're using in the form (org | user)/repo"
