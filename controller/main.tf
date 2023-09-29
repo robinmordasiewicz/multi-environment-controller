3e86ed0d2af6abd5a2c21b737c36593bf3ea2f2f
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-09-28 23:26:03 -0400

    updating fortigate config

--- controller/main.tf
+++ controller/main.tf
@@ -40,21 +40,57 @@ resource "azurerm_storage_container" "AZURE_TFSTATE_CONTAINER" {
   storage_account_name = azurerm_storage_account.TFSTATE_STORAGE_ACCOUNT[each.key].name
 }
 
-module "AZURE_SERVICE_PRINCIPAL" {
-  for_each         = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
-  environment_name = base64encode(each.value.REPOSITORY_FULL_NAME)
-  #identity_name    = replace(data.github_repository.REPOSITORY[each.value].full_name, '/', '-')
-  identity_name   = data.github_repository.REPOSITORY[each.key].name
-  source          = "ned1313/github_oidc/azuread"
-  version         = ">=1.2.0"
-  entity_type     = "environment"
-  repository_name = data.github_repository.REPOSITORY[each.key].full_name
+data "azuread_application_published_app_ids" "well_known" {}
+
+data "azuread_service_principal" "msgraph" {
+  application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
 }
 
+resource "azuread_application" "AZURE_APPLICATION" {
+  for_each     = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
+  display_name = replace(data.github_repository.REPOSITORY[each.key].full_name, "/", "-")
+  owners       = [data.azuread_client_config.current.object_id]
+  required_resource_access {
+    resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+    resource_access {
+      id   = data.azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
+      type = "Role"
+    }
+    resource_access {
+      id   = data.azuread_service_principal.msgraph.app_role_ids["Group.ReadWrite.All"]
+      type = "Role"
+    }
+    resource_access {
+      id   = data.azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
+      type = "Role"
+    }
+  }
+}
+
+resource "azuread_application_federated_identity_credential" "AZURE_FEDERATED_IDENTITY" {
+  for_each              = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
+  application_object_id = azuread_application.AZURE_APPLICATION[each.key].object_id
+  display_name          = azuread_application.AZURE_APPLICATION[each.key].display_name
+  description           = "GitHub OIDC for ${data.github_repository.REPOSITORY[each.key].full_name}."
+  audiences             = ["api://AzureADTokenExchange"]
+  issuer                = "https://token.actions.githubusercontent.com"
+  subject               = "repo:${data.github_repository.REPOSITORY[each.key].full_name}:environment:${github_repository_environment.REPOSITORY_FULL_NAME[each.key].environment}"
+}
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_service_principal" "AZURE_SERVICE_PRINCIPAL" {
+  for_each       = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
+  application_id = azuread_application.AZURE_APPLICATION[each.key].application_id
+  owners         = [data.azuread_client_config.current.object_id]
+}
+
+#resource "null_resource" "admin_consent" {
+
 resource "azurerm_role_assignment" "ROLE_ASSIGNMENT" {
-  for_each             = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
+  for_each = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
   #scope                = azurerm_resource_group.TFSTATE_RESOURCE_GROUP[each.key].id
-  scope              = data.azurerm_subscription.subscription[each.key].id
+  scope                = data.azurerm_subscription.subscription[each.key].id
   role_definition_name = "ADMINISTRATOR_ROLE"
   principal_id         = module.AZURE_SERVICE_PRINCIPAL[each.key].service_principal.object_id
 }
@@ -62,7 +98,7 @@ resource "azurerm_role_assignment" "ROLE_ASSIGNMENT" {
 data "azuread_client_config" "current" {}
 
 data "azurerm_subscription" "subscription" {
-  for_each             = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
+  for_each        = { for application in var.applications : application.REPOSITORY_FULL_NAME => application }
   subscription_id = each.value.ARM_SUBSCRIPTION_ID
 }
 
