4cf51cca19dbd5a2062a6b817d890f9cf720c08c
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-23 17:11:38 -0400

    getting megalinter working

--- application/terraform.tf
+++ application/terraform.tf
@@ -5,31 +5,32 @@ terraform {
       source  = "integrations/github"
       version = "~>5.34.0"
     }
-
     azuread = {
       source  = "hashicorp/azuread"
       version = "~>2.41.0"
     }
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.71.0"
     }
-
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~>3.5.1"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.2.1"
     }
   }
-  backend "azurerm" {
-  }
+  # backend "azurerm" {}
 }
 
 provider "github" {}
+provider "tls" {}
 
 provider "azurerm" {
   features {
