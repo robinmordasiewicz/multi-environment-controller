45c2faddafa1e18690044277a5c2d278c071f305
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-10-31 00:44:07 -0400

    fixing permissions

--- application/main.tf
+++ application/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_storage_account" "tfstate_storage_account" {
   location            = azurerm_resource_group.azure_resource_group[each.key].location
   name                = "${random_integer.random_number[each.key].result}${lower(each.key)}"
   account_tier        = "Standard"
-  #account_replication_type      = "LRS"
+  account_replication_type      = "LRS"
   enable_https_traffic_only = true
   #min_tls_version               = "TLS1_2"
   #public_network_access_enabled = false
